b5222a2c592c24e276d0da24d9d54b8a23b407e9
Vinicius <viniciusiandrade@hotmail.com>
2022-07-31 19:22:21 -0300

    MUI Icons dependency

--- components/MenuOptions.js
+++ components/MenuOptions.js
@@ -1,13 +1,17 @@
-import React from 'react'
 import styles from '../styles/MenuOptions.module.css'
+import HomeIcon from '@mui/icons-material/Home';
+import AppsIcon from '@mui/icons-material/Apps';
+import InfoIcon from '@mui/icons-material/Info';
+import PhoneIcon from '@mui/icons-material/Phone';
 
 const MenuOptions = ({ isActive }) => {
+
   return (
       <ul className={`${styles.wrapperOptions}  ${isActive && styles.active}` }>
-          <li className={styles.li} title=''> 1 </li>
-          <li className={styles.li} title=''> 2 </li>
-          <li className={styles.li} title=''> 3 </li>
-          <li className={styles.li} title=''> 4 </li>
+          <li  className={styles.li} title='Home'> <HomeIcon/> </li>
+          <li  className={styles.li} title='Quem somos'> <InfoIcon/> </li>
+          <li  className={styles.li} title='Contato'> <PhoneIcon/> </li>
+          <li  className={styles.li} title='Aplicações'> <AppsIcon/> </li>
       </ul>
   )
 }
